f1d713711682024f0de3d5f54b27f9b42e4fb532
Łukasz W <wolanczyk.lukasz@gmail.com>
2021-06-28 09:12:36 +0200

    Extract default screen functions for easier reuse

--- src/screen.rs
+++ src/screen.rs
@@ -21,44 +21,102 @@ where
         message: ScreenMessage,
         clipboard: &mut iced::Clipboard,
     ) -> Command<Message<RouteMessage, ScreenMessage>> {
-        Command::batch(
-            self.child_screens()
-                .iter_mut()
-                .map(|screen| screen.update(message.clone(), clipboard)),
-        )
+        update_children(self, message, clipboard)
     }
 
     fn on_create(&mut self) -> Command<Message<RouteMessage, ScreenMessage>> {
-        Command::batch(
-            self.child_screens()
-                .iter_mut()
-                .map(|screen| screen.on_create()),
-        )
+        on_create_children(self)
     }
 
     fn on_present(&mut self) -> Command<Message<RouteMessage, ScreenMessage>> {
-        Command::batch(
-            self.child_screens()
-                .iter_mut()
-                .map(|screen| screen.on_present()),
-        )
+        on_present_children(self)
     }
 
     fn on_stop_presenting(&mut self) -> Command<Message<RouteMessage, ScreenMessage>> {
-        Command::batch(
-            self.child_screens()
-                .iter_mut()
-                .map(|screen| screen.on_stop_presenting()),
-        )
+        on_stop_presenting_children(self)
     }
 
     fn on_dismiss(&mut self) -> Command<Message<RouteMessage, ScreenMessage>> {
-        Command::batch(
-            self.child_screens()
-                .iter_mut()
-                .map(|screen| screen.on_dismiss()),
-        )
+        on_dismiss_children(self)
     }
 
     fn view(&mut self) -> Element<Message<RouteMessage, ScreenMessage>>;
+}
+
+pub fn update_children<T, RouteMessage, ScreenMessage>(
+    screen: &mut T,
+    message: ScreenMessage,
+    clipboard: &mut iced::Clipboard,
+) -> Command<Message<RouteMessage, ScreenMessage>>
+where
+    T: Screen<RouteMessage, ScreenMessage> + ?Sized,
+    ScreenMessage: Clone,
+{
+    println!("Calling update children!");
+    Command::batch(
+        screen
+            .child_screens()
+            .iter_mut()
+            .map(|screen| screen.update(message.clone(), clipboard)),
+    )
+}
+
+pub fn on_create_children<T, RouteMessage, ScreenMessage>(
+    screen: &mut T,
+) -> Command<Message<RouteMessage, ScreenMessage>>
+where
+    T: Screen<RouteMessage, ScreenMessage> + ?Sized,
+    ScreenMessage: Clone,
+{
+    Command::batch(
+        screen
+            .child_screens()
+            .iter_mut()
+            .map(|screen| screen.on_create()),
+    )
+}
+
+pub fn on_present_children<T, RouteMessage, ScreenMessage>(
+    screen: &mut T,
+) -> Command<Message<RouteMessage, ScreenMessage>>
+where
+    T: Screen<RouteMessage, ScreenMessage> + ?Sized,
+    ScreenMessage: Clone,
+{
+    Command::batch(
+        screen
+            .child_screens()
+            .iter_mut()
+            .map(|screen| screen.on_present()),
+    )
+}
+
+pub fn on_stop_presenting_children<T, RouteMessage, ScreenMessage>(
+    screen: &mut T,
+) -> Command<Message<RouteMessage, ScreenMessage>>
+where
+    T: Screen<RouteMessage, ScreenMessage> + ?Sized,
+    ScreenMessage: Clone,
+{
+    Command::batch(
+        screen
+            .child_screens()
+            .iter_mut()
+            .map(|screen| screen.on_stop_presenting()),
+    )
+}
+
+pub fn on_dismiss_children<T, RouteMessage, ScreenMessage>(
+    screen: &mut T,
+) -> Command<Message<RouteMessage, ScreenMessage>>
+where
+    T: Screen<RouteMessage, ScreenMessage> + ?Sized,
+    ScreenMessage: Clone,
+{
+    Command::batch(
+        screen
+            .child_screens()
+            .iter_mut()
+            .map(|screen| screen.on_dismiss()),
+    )
 }
